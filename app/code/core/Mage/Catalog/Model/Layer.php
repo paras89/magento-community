6d8a7b44856b419fbd774457b7eb4fa7c66b8fef
Chris Vander Poel <cvp@alexanderinteractive.com>
2014-03-12 18:19:10 -0400

    Initial commit of the 1.1.8 branch.

---
+++ app/code/core/Mage/Catalog/Model/Layer.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Mage
+ * @package    Mage_Catalog
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+/**
+ * Catalog view layer model
+ *
+ * @category   Mage
+ * @package    Mage_Catalog
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Catalog_Model_Layer extends Varien_Object
+{
+    protected $_productCollections = array();
+
+    /**
+     * Retrieve current layer product collection
+     *
+     * @return Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Collection
+     */
+    public function getProductCollection()
+    {
+        if (isset($this->_productCollections[$this->getCurrentCategory()->getId()])) {
+            $collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
+        }
+        else {
+            $collection = $this->getCurrentCategory()->getProductCollection();
+            $this->prepareProductCollection($collection);
+            $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Initialize product collection
+     *
+     * @param Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Collection $collection
+     * @return Mage_Catalog_Model_Layer
+     */
+    public function prepareProductCollection($collection)
+    {
+        $collection->addAttributeToSelect(Mage::getSingleton('catalog/config')->getProductAttributes())
+            ->addMinimalPrice()
+            ->addFinalPrice()
+            ->addTaxPercents()
+            ->addStoreFilter();
+
+        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
+        $collection->addUrlRewrite($this->getCurrentCategory()->getId());
+
+        return $this;
+    }
+
+    /**
+     * Apply layer
+     * Method is colling after apply all filters, can be used
+     * for prepare some index data before getting information
+     * about existing intexes
+     *
+     * @return Mage_Catalog_Model_Layer
+     */
+    public function apply()
+    {
+        return $this;
+    }
+
+    /**
+     * Retrieve current category model
+     * If no category found in registry, the root will be taken
+     *
+     * @return Mage_Catalog_Model_Category
+     */
+    public function getCurrentCategory()
+    {
+        $category = $this->getData('current_category');
+        if (is_null($category)) {
+            if ($category = Mage::registry('current_category')) {
+                $this->setData('current_category', $category);
+            }
+            else {
+                $category = Mage::getModel('catalog/category')->load($this->getCurrentStore()->getRootCategoryId());
+                $this->setData('current_category', $category);
+            }
+        }
+        return $category;
+    }
+
+    /**
+     * Change current category object
+     *
+     * @param mixed $category
+     * @return Mage_Catalog_Model_Layer
+     */
+    public function setCurrentCategory($category)
+    {
+        if (is_numeric($category)) {
+            $category = Mage::getModel('catalog/category')->load($category);
+        }
+        if (!$category instanceof Mage_Catalog_Model_Category) {
+            Mage::throwException(Mage::helper('catalog')->__('Category must be instance of Mage_Catalog_Model_Category'));
+        }
+        if (!$category->getId()) {
+            Mage::throwException(Mage::helper('catalog')->__('Invalid category'));
+        }
+
+        if ($category->getId() != $this->getCurrentCategory()->getId()) {
+            $this->setData('current_category', $category);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieve current store model
+     *
+     * @return Mage_Core_Model_Store
+     */
+    public function getCurrentStore()
+    {
+        return Mage::app()->getStore();
+    }
+
+    /**
+     * Enter description here...
+     *
+     * @return Mage_Eav_Model_Mysql4_Entity_Attribute_Collection
+     */
+    public function getFilterableAttributes()
+    {
+        $entity = $this->getProductCollection()->getEntity();
+        $setIds = $this->getProductCollection()->getSetIds();
+
+        if (!$setIds)
+            return array();
+
+        $collection = Mage::getModel('eav/entity_attribute')->getCollection()
+            ->setItemObjectClass('catalog/resource_eav_attribute');
+        /* @var $collection Mage_Eav_Model_Mysql4_Entity_Attribute_Collection */
+        $collection->getSelect()->distinct(true);
+        $collection->setEntityTypeFilter($entity->getTypeId())
+            ->setAttributeSetFilter($setIds)
+            ->addIsFilterableFilter()
+            ->setOrder('position', 'ASC')
+            ->load();
+
+        foreach ($collection as $item) {
+            Mage::getResourceSingleton('catalog/product')->getAttribute($item);
+            $item->setEntity($entity);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Retrieve layer state object
+     *
+     * @return Mage_Catalog_Model_Layer_State
+     */
+    public function getState()
+    {
+        $state = $this->getData('state');
+        if (is_null($state)) {
+            $state = Mage::getModel('catalog/layer_state');
+            $this->setData('state', $state);
+        }
+        return $state;
+    }
+
+}
---
+++ app/code/core/Mage/CatalogSearch/Block/Result.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Mage
+ * @package    Mage_CatalogSearch
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+/**
+ * Product search result block
+ *
+ * @category   Mage
+ * @package    Mage_CatalogSearch
+ * @module     Catalog
+ */
+class Mage_CatalogSearch_Block_Result extends Mage_Core_Block_Template
+{
+    protected $_productCollection;
+
+    protected function _getQuery()
+    {
+        return $this->helper('catalogSearch')->getQuery();
+    }
+
+    protected function _prepareLayout()
+    {
+        $title = $this->__("Search results for: '%s'", $this->helper('catalogSearch')->getEscapedQueryText());
+
+        // add Home breadcrumb
+        if ($breadcrumbs = $this->getLayout()->getBlock('breadcrumbs')) {
+            $breadcrumbs->addCrumb('home', array(
+                'label' => $this->__('Home'),
+                'title' => $this->__('Go to Home Page'),
+                'link'  => Mage::getBaseUrl()
+            ))->addCrumb('search', array(
+                'label' => $title,
+                'title' => $title
+            ));
+        }
+        $this->getLayout()->getBlock('head')->setTitle($title);
+
+        return parent::_prepareLayout();
+    }
+
+    public function setListOrders() {
+        $this->getChild('search_result_list')
+            ->setAvailableOrders(array(
+                'name'  => $this->__('Name'),
+                'price' => $this->__('Price'))
+            );
+    }
+
+    public function setListModes() {
+        $this->getChild('search_result_list')
+            ->setModes(array(
+                'grid' => $this->__('Grid'),
+                'list' => $this->__('List'))
+            );
+    }
+
+    public function setListCollection() {
+        $this->getChild('search_result_list')
+           ->setCollection($this->_getProductCollection());
+    }
+
+    public function getProductListHtml()
+    {
+        return $this->getChildHtml('search_result_list');
+    }
+
+    /**
+     * Retrieve loaded category collection
+     *
+     * @return Mage_Eav_Model_Entity_Collection_Abstract
+     */
+    protected function _getProductCollection()
+    {
+        if (is_null($this->_productCollection)) {
+            $this->_productCollection = $this->_getQuery()->getResultCollection()
+                ->addAttributeToSelect(Mage::getSingleton('catalog/config')->getProductAttributes());
+
+            Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($this->_productCollection);
+            Mage::getSingleton('catalog/product_visibility')->addVisibleInSearchFilterToCollection($this->_productCollection);
+        }
+
+        return $this->_productCollection;
+    }
+
+    public function getResultCount()
+    {
+        if (!$this->getData('result_count')) {
+            $size = $this->_getProductCollection()->getSize();
+            $this->_getQuery()->setNumResults($size);
+            $this->setResultCount($size);
+        }
+        return $this->getData('result_count');
+    }
+}
