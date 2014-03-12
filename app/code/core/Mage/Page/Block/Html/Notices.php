bfe45c153ae91c8f363225cf6f826c1b4c0b0a53
Chris Vander Poel <cvp@alexanderinteractive.com>
2014-03-12 15:27:37 -0400

    Initial commit of the 1.7.0.0-beta1 branch.

---
+++ app/code/core/Mage/Page/Block/Html/Notices.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Page
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Html notices block
+ *
+ * @category    Mage
+ * @package     Mage_Page
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Mage_Page_Block_Html_Notices extends Mage_Core_Block_Template
+{
+    /**
+     * Path to configuration, check is enable cookie restriction mode
+     */
+    const XML_PATH_COOKIE_RESTRICTION  = 'web/cookie/cookie_restriction';
+
+    /**
+     * Check if noscript notice should be displayed
+     *
+     * @return boolean
+     */
+    public function displayNoscriptNotice()
+    {
+        return Mage::getStoreConfig('web/browser_capabilities/javascript');
+    }
+
+    /**
+     * Check if demo store notice should be displayed
+     *
+     * @return boolean
+     */
+    public function displayDemoNotice()
+    {
+        return Mage::getStoreConfig('design/head/demonotice');
+    }
+
+    /**
+     * Check if cookie restriction notice should be displayed
+     *
+     * @return bool
+     */
+    public function displayCookieRestrictionNotice()
+    {
+        $acceptedSaveCookiesWebsites = $this->_getAcceptedSaveCookiesWebsites();
+        return Mage::getStoreConfig(self::XML_PATH_COOKIE_RESTRICTION) &&
+            empty($acceptedSaveCookiesWebsites[Mage::app()->getWebsite()->getId()]);
+    }
+
+    /**
+     * Get Link to cookie restriction privacy policy page
+     *
+     * @return string
+     */
+    public function getPrivacyPolicyLink()
+    {
+        return Mage::getUrl('privacy-policy-cookie-restriction-mode');
+    }
+
+    /**
+     * Return serialzed list of accepted save cookie website
+     *
+     * @return string
+     */
+    public function getAcceptedSaveCookiesWebsiteIds()
+    {
+        $acceptedSaveCookiesWebsites = $this->_getAcceptedSaveCookiesWebsites();
+        $acceptedSaveCookiesWebsites[Mage::app()->getWebsite()->getId()] = 1;
+        return serialize($acceptedSaveCookiesWebsites);
+    }
+
+    /**
+     * Get accepted save cookies websites
+     *
+     * @return array
+     */
+    protected function _getAcceptedSaveCookiesWebsites()
+    {
+        $serializedList = Mage::getSingleton('core/cookie')->get(Mage_Page_Helper_Data::IS_USER_ALLOWED_SAVE_COOKIE);
+        $unSerializedList = unserialize($serializedList);
+        return is_array($unSerializedList) ? $unSerializedList : array();
+    }
+}
